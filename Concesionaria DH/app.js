33259ba6d4dc218e96074c369656d2cf709ba5e3
eDaNi-M <danimiguel9626@gmail.com>
2024-03-22 13:44:57 -0300

    cargandolo en un repo

---
+++ Concesionaria DH/app.js
@@ -0,0 +1,39 @@
+const autosImportados = require("./autos.js");
+
+const concesionaria = {
+  autos: autosImportados,
+  buscarAuto(patente){
+    return this.autos.find(auto => auto.patente === patente) || null;
+  },
+  venderAuto(patente){
+    const auto = this.buscarAuto(patente);
+    return auto ? (auto.vendido = true, auto) : null;
+  },
+  autosParaLaVenta(){
+    return this.autos.filter(auto => !auto.vendido);
+  },
+  autosNuevos(){
+    return this.autosParaLaVenta().filter(auto => auto.km < 100);
+  },
+  listaDeVentas(){
+    return this.autos.filter(auto => auto.vendido).map(auto => auto.precio);
+  },
+  totalDeVentas(){
+    return this.listaDeVentas().reduce((acumulador, venta) => acumulador + venta, 0);
+  },
+  puedeComprar(auto, persona){
+    return auto.precio <= persona.capacidadDePagoTotal && (auto.precio / auto.cuotas) <= persona.capacidadDePagoEnCuotas;
+  },
+  autosQuePuedeComprar(persona){
+    return this.autosParaLaVenta().filter(auto => this.puedeComprar(auto, persona));
+  }
+};
+
+let persona = {
+    nombre : "Juan",
+    capacidadDePagoEnCuotas: 20000,
+    capacidadDePagoTotal: 100000
+}
+
+console.log(concesionaria.autosQuePuedeComprar(persona));
+
